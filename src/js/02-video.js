b027603a08321fd9e6978cd5dc242879bde015ed
OlegKravinskyy <kravinskyy@gmail.com>
2022-09-09 01:04:47 +0300

    added task-3 feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -28,5 +28,3 @@ player
         break;
     }
   });
-
-console.log();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+feedbackForm = document.querySelector('.feedback-form');
+
+const formData = {};
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onTextareaInput, 500));
+
+onGetFormData();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function onTextareaInput(e) {
+  formData.email = feedbackForm.email.value;
+  formData.message = feedbackForm.message.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onGetFormData() {
+  const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (saveData) {
+    feedbackForm.email.value = saveData.email;
+    feedbackForm.message.value = saveData.message;
+  }
+}
